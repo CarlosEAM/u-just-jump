ba0dc74bc04e56a07d0cbf61e0c7e43c6bc43ac6
Carlos E Alford M <carlos.alford@gmail.com>
2020-06-05 14:14:01 +0100

    feat: Motion and Collision

--- main.js
+++ main.js
@@ -289,8 +289,89 @@ DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
 
 
 
-
+// TEST DRAWING, delete when done
 let simpleLevel = new Level(simpleLevelPlan);
 let display = new DOMDisplay(document.body, simpleLevel);
 display.syncState(State.start(simpleLevel));
+
+
+// MOTION AND COLLISION
+// Split time into small steps and for each step, move the actors
+// by a distance corresponding to their speed multiplied by the size
+// of time step. Units per second.
+
+// This method tells us whether a rectangle (specified by a position and a size)
+// touches a grid element of the given type.
+Level.prototype.touches = function(pos, size, type) {
+  var xStart = Math.floor(pos.x);
+  var xEnd = Math.ceil(pos.x + size.x);
+  var yStart = Math.floor(pos.y);
+  var yEnd = Math.ceil(pos.y + size.y);
+  for (var y = yStart; y < yEnd; y++) {
+    for (var x = xStart; x < xEnd; x++) {
+      let isOutside = x < 0 || x >= this.width || y < 0 || y >= this.height;
+      let here = isOutside ? "wall" : this.rows[y][x];
+      if (here == type) return true;
+    }
+  }
+  return false;
+};
+
+
+
+
+// The state update method uses touches to figure out whether the player is
+// touching lava.
+State.prototype.update = function(time, keys) {
+  let actors = this.actors.map(actor => actor.update(time, this, keys));
+  let newState = new State(this.level, actors, this.status);
+  if (newState.status != "playing") return newState;
+  let player = newState.player;
+  if (this.level.touches(player.pos, player.size, "lava")) {
+    return new State(this.level, actors, "lost");
+  }
+  for (let actor of actors) {
+    if (actor != player && overlap(actor, player)) {
+      newState = actor.collide(newState);
+    }
+  }
+  return newState;
+}
+
+
+
+
+
+
+// Overlap between actors is detected with the overlap function.
+function overlap(actor1, actor2) {
+  return actor1.pos.x + actor1.size.x > actor2.pos.x &&
+    actor1.pos.x < actor2.pos.x + actor2.size.x &&
+    actor1.pos.y + actor1.size.y > actor2.pos.y &&
+    actor1.pos.y < actor2.pos.y + actor2.size.y;
+}
+
+
+
+
+Lava.prototype.collide = function(state) {
+  return new State(state.level, state.actors, "lost");
+};
+
+Coin.prototype.collide = function(state) {
+  let filtered = state.actors.filter(a => a != this);
+  let status = state.status;
+  if (!filtered.some(a => a.type == "coin")) status = "won";
+  return new State(state.level, filtered, status);
+};
+
+
+
+
+
+
+
+
+
+
 
